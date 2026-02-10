32a4ae193392533c232ef0707ce299e272571179
YashTen80 <yasasvi@rayvector.com>
2026-02-10 17:41:36 +0530

    Camera bounds adjustments made; player bounding box issue remaining

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,18 +9,22 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform playerPos;
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 startingOffset;
     [SerializeField] float initalXPos;
     [SerializeField] private float translationOffset;
     private float horizontalUpperBound;
+    private Camera cam;
+    private readonly float aspectRatio = 16 / 9;
     // Start is called before the first frame update
     void Start()
     {
         playerPos = GameObject.Find("Player").transform;
-        transform.position += offset;
+        transform.position = startingOffset;
 
-        horizontalUpperBound = transform.position.x + MathF.Abs(transform.GetComponent<Camera>().orthographicSize / 2);
-        Debug.Log("Horizontal Uppser bound: " + horizontalUpperBound);
+        cam = transform.GetComponent<Camera>();
+
+        horizontalUpperBound = transform.position.x + cam.orthographicSize * aspectRatio;
+        Debug.Log("Horizontal Uppser bound: " + transform.GetComponent<Camera>().orthographicSize);
     }
 
     // Update is called once per frame
@@ -31,12 +35,35 @@ public class CameraController : MonoBehaviour
 
     private void CameraPosition()
     {
-        Vector2 playerPosition = playerPos.position;
+        float playerPosition = playerPos.GetComponent<Renderer>().bounds.max.x;
         //transform.position = new Vector3(playerPosition.x, transform.position.y, transform.position.z) + offset;
         //float horizontalUpperBound =
-        if (playerPosition.x >= transform.position.x)
+        if (playerPosition >= horizontalUpperBound)
         {
+            Debug.Log("Playerreached end of camera range");
             //transform.position = new Vector3(transform.position.x + translationOffset, transform.position.y, transform.position.z);
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!cam || !cam.orthographic) return;
+
+        float halfWidth = cam.orthographicSize * cam.aspect;
+
+        float rightBound = cam.transform.position.x + halfWidth;
+        float leftBound = cam.transform.position.x - halfWidth;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(
+            new Vector3(rightBound, -1000f, 0),
+            new Vector3(rightBound, 1000f, 0)
+        );
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(
+            new Vector3(leftBound, -1000f, 0),
+            new Vector3(leftBound, 1000f, 0)
+        );
     }
 }
